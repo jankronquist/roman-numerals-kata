92f265dd919e814b197a806a759afacf9896c0c5
Erik Underbjerg <erik@underbjerg.com>
2014-05-09 16:21:19 +0200

    implemented javascript stuff

--- busterjs/lib/roman-numerals.js
+++ busterjs/lib/roman-numerals.js
@@ -1,4 +1,38 @@
+function decimalToRomanDigit(value, one, five, ten) {
+	switch(value) {
+	case 0:
+		return "";
+	case 1:
+		return one;
+	case 2:
+		return one + one;
+	case 3:
+		return one + one + one;
+	case 4:
+		return one + five;
+	case 5:
+		return five;
+	case 6:
+		return five + one;
+	case 7:
+		return five + one + one;
+	case 8:
+		return five + one + one + one;
+	case 9:
+		return one + ten;
+	case 10:
+		return ten;
+	default:
+		return "ERROR";
+	}
+	return "NOOOO";
+}
+
 function decimalToRoman(number) {
+	return decimalToRomanDigit( Math.floor((number % 10000)/1000), "M", "D", "M") 
+		 	+ decimalToRomanDigit( Math.floor((number % 1000)/100), "C", "D", "M") 
+	 		+ decimalToRomanDigit( Math.floor((number % 100)/10), "X", "L", "C") 
+			+ decimalToRomanDigit(number % 10, "I", "V", "X");
 }
 
 module.exports = decimalToRoman;
--- busterjs/test/roman-numerals-test.js
+++ busterjs/test/roman-numerals-test.js
@@ -22,5 +22,35 @@ buster.spec.expose();
 buster.testCase('decimalToRoman', {
     'returns I for 1': function () {
         assert.equals(decimalToRoman(1), 'I');
+    },
+    'returns II for 2': function () {
+        assert.equals(decimalToRoman(2), 'II');
+    },
+    'returns IV for 4': function () {
+        assert.equals(decimalToRoman(4), 'IV');
+    },
+    'returns V for 5': function () {
+        assert.equals(decimalToRoman(5), 'V');
+    },
+    'returns XXXIV for 34': function () {
+        assert.equals(decimalToRoman(34), 'XXXIV');
+    },
+    'returns XL for 40': function () {
+        assert.equals(decimalToRoman(40), 'XL');
+    },
+    'returns XLIX for 49': function () {
+        assert.equals(decimalToRoman(49), 'XLIX');
+    },
+    'returns XC for 90': function () {
+        assert.equals(decimalToRoman(90), 'XC');
+    },
+    'returns XCIX for 99': function () {
+        assert.equals(decimalToRoman(99), 'XCIX');
+    },
+    'returns CXI for 111': function () {
+        assert.equals(decimalToRoman(111), 'CXI');
+    },
+    'returns MMMCMXCIV for 3994': function () {
+        assert.equals(decimalToRoman(3994), 'MMMCMXCIV');
     }
 });
